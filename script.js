43fbcdb55018c2901035219173323618f9082c0d
ThomasKristensen05 <thomas12andre@gmail.com>
2023-02-07 10:59:09 +0100

    opgave 1

---
+++ script.js
@@ -0,0 +1,16 @@
+let biler = {
+    Nissan: "r34",
+    Mercedes: "m5",
+    McLaren: "p1",
+    Pagani: "zonda r"
+}
+
+console.log(biler.Nissan)
+console.log(biler.Mercedes)
+console.log(biler.McLaren)
+console.log(biler.Pagani)
+
+console.log(biler["Nissan"])
+console.log(biler["Mercedes"])
+console.log(biler["McLaren"])
+console.log(biler["Pagani"])
